1c0d9f98ee4fefbbc135532014cb112f25c0691c
jeremybdavis <jdavis129@gmail.com>
2016-10-08 16:42:41 -0500

    contact section and footer added for mobile homepage

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,17 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">Jeremy Davis</a>
+
+    <nav>
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
+
+    <div class="footer-social">
+      Social
+    </div>
   </div>
 </footer>
--- templates/home/content-home.php
+++ templates/home/content-home.php
@@ -1,5 +1,13 @@
 <?php use Roots\Sage\Titles; ?>
 
+<style>
+<?php if(get_field('home_contact_bg_img')): ?>
+  .home-container .home-contact {
+    background-image: url(<?php the_field('home_contact_bg_img') ?>);
+  }
+<?php endif; ?>
+</style>
+
 <div class="home-featured">
   <?php if(get_field('home_featured_img_1')): ?>
     <img src="<?php the_field('home_featured_img_1') ?>" alt="Bio" class="col-md-6" />
@@ -19,5 +27,12 @@
   <div class="home-copy col-md-6">
     <h3>Lorem Ipsum</h3>
     <a href="#" class="btn btn-primary">View Portfolio</a>
+  </div>
+</div>
+
+<div class="home-contact">
+  <div class="home-copy">
+    <h3>Lorem Ipsum</h3>
+    <a href="#" class="btn btn-primary">Contact Me</a>
   </div>
 </div>
