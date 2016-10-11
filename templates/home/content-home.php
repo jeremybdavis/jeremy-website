e70c3047a82bd9155b9e389efd67084b3656f729
jeremybdavis <jdavis129@gmail.com>
2016-10-11 01:19:26 -0500

    all acf fields added on homepage, began styling for desktop homepage layout

--- templates/home/content-home.php
+++ templates/home/content-home.php
@@ -14,8 +14,16 @@
   <?php endif; ?>
 
   <div class="home-copy col-md-6">
-    <h3>Lorem Ipsum</h3>
-    <a href="#" class="btn btn-primary">View Bio</a>
+    <?php if( get_field('home_featured_headline_1')): ?>
+    <h3><?php the_field('home_featured_headline_1') ?></h3>
+    <?php endif; ?>
+    <a href="#" class="btn btn-primary">
+      <?php if( get_field('home_featured_cta_1') ) {
+        echo the_field('home_featured_cta_1');
+      } else {
+        echo 'View More';
+      } ?>
+    </a>
   </div>
 </div>
 
@@ -25,14 +33,30 @@
   <?php endif; ?>
 
   <div class="home-copy col-md-6">
-    <h3>Lorem Ipsum</h3>
-    <a href="#" class="btn btn-primary">View Portfolio</a>
+    <?php if( get_field('home_featured_headline_2')): ?>
+      <h3><?php the_field('home_featured_headline_2') ?></h3>
+    <?php endif; ?>
+    <a href="#" class="btn btn-primary">
+      <?php if( get_field('home_featured_cta_2') ) {
+        echo the_field('home_featured_cta_2');
+      } else {
+        echo 'View More';
+      } ?>
+    </a>
   </div>
 </div>
 
 <div class="home-contact">
   <div class="home-copy">
-    <h3>Lorem Ipsum</h3>
-    <a href="#" class="btn btn-primary">Contact Me</a>
+    <?php if( get_field('home_contact_headline')): ?>
+    <h3><?php the_field('home_contact_headline') ?></h3>
+    <?php endif; ?>
+    <a href="#" class="btn btn-primary">
+      <?php if( get_field('home_contact_cta') ) {
+        echo the_field('home_contact_cta');
+      } else {
+        echo 'Contact Me';
+      } ?>
+    </a>
   </div>
 </div>
